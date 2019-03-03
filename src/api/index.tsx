1070562deefc9bdbb0518698eab5b76640387e74
paul_melero <paulmelero@gmail>
2019-03-03 08:06:03 +0100

    [master] removed files and added initial setup

---
+++ src/api/index.tsx
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+const footBallApiInstance = axios.create({
+  baseURL: 'http://api.football-data.org/v2/',
+  headers: {
+    'X-Auth-Token': process.env.REACT_APP_API_KEY,
+  }
+});
+
+export default footBallApiInstance;
---
+++ src/api/store/actions.ts
@@ -0,0 +1,3 @@
+import footBallApi from 'src/api';
+
+export const REQUEST_STANDINGS = 'REQUEST_STANDINGS'
