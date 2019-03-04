a20ce753973a7b96b9c9e828ec183ded0fc3477e
paul_melero <paulmelero@gmail>
2019-03-04 14:42:30 +0100

    [master] fixed mixed content error

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const footBallApiInstance = axios.create({
-  baseURL: 'http://api.football-data.org/v2/',
+  baseURL: 'https://api.football-data.org/v2/',
   headers: {
     'X-Auth-Token': process.env.REACT_APP_API_KEY,
   }
